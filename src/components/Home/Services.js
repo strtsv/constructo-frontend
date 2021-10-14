5152d7ef64fb96b7957fc39d91bcc94085fba1f4
jsusilo <enr@bk.ru>
2021-10-14 23:59:04 +0500

    home services

---
+++ src/components/Home/Services.js
@@ -0,0 +1,152 @@
+import React from "react";
+
+class Services extends React.Component {
+  render() {
+    return (
+      <section className="section-md bg-default text-center" id="services">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <h4 className="heading-decorated">Our Services</h4>
+            </div>
+          </div>
+          <div className="row row-50 justify-content-md-center justify-content-lg-start">
+            <div className="col-md-6 col-xl-4">
+              {}
+              <article className="blurb blurb-circle">
+                <div className="unit flex-sm-row unit-spacing-md">
+                  <div className="unit__left">
+                    <div className="blurb-circle__icon">
+                      <span className="icon linear-icon-pencil-ruler" />
+                    </div>
+                  </div>
+                  <div className="unit__body">
+                    <p className="heading-6 blurb__title">
+                      <a href="#">Pre-construction Services</a>
+                    </p>
+                    <p>
+                      We take our time on initial planning before any
+                      construction begins, to balance all the financial and
+                      efficiency issues beforehand...
+                    </p>
+                  </div>
+                </div>
+              </article>
+            </div>
+            <div className="col-md-6 col-xl-4">
+              {}
+              <article className="blurb blurb-circle">
+                <div className="unit flex-sm-row unit-spacing-md">
+                  <div className="unit__left">
+                    <div className="blurb-circle__icon">
+                      <span className="icon linear-icon-users" />
+                    </div>
+                  </div>
+                  <div className="unit__body">
+                    <p className="heading-6 blurb__title">
+                      <a href="#">General Contracting</a>
+                    </p>
+                    <p>
+                      We have a long list of professional contractors, whom our
+                      engineers and architects enjoy to work with on a majority
+                      of our projects!
+                    </p>
+                  </div>
+                </div>
+              </article>
+            </div>
+            <div className="col-md-6 col-xl-4">
+              {}
+              <article className="blurb blurb-circle">
+                <div className="unit flex-sm-row unit-spacing-md">
+                  <div className="unit__left">
+                    <div className="blurb-circle__icon">
+                      <span className="icon linear-icon-wall" />
+                    </div>
+                  </div>
+                  <div className="unit__body">
+                    <p className="heading-6 blurb__title">
+                      <a href="#">Construction Services</a>
+                    </p>
+                    <p>
+                      Our customers love the pace/quality tempo that we show
+                      during each of the principal construction processes!
+                    </p>
+                  </div>
+                </div>
+              </article>
+            </div>
+            <div className="col-md-6 col-xl-4">
+              {}
+              <article className="blurb blurb-circle">
+                <div className="unit flex-sm-row unit-spacing-md">
+                  <div className="unit__left">
+                    <div className="blurb-circle__icon">
+                      <span className="icon linear-icon-apartment" />
+                    </div>
+                  </div>
+                  <div className="unit__body">
+                    <p className="heading-6 blurb__title">
+                      <a href="#">Construction Management</a>
+                    </p>
+                    <p>
+                      Construction project management is essential. We're using
+                      the most time and iterations efficient life cycles methods
+                      for that.
+                    </p>
+                  </div>
+                </div>
+              </article>
+            </div>
+            <div className="col-md-6 col-xl-4">
+              {}
+              <article className="blurb blurb-circle">
+                <div className="unit flex-sm-row unit-spacing-md">
+                  <div className="unit__left">
+                    <div className="blurb-circle__icon">
+                      <span className="icon linear-icon-home4" />
+                    </div>
+                  </div>
+                  <div className="unit__body">
+                    <p className="heading-6 blurb__title">
+                      <a href="#">Building Information Modeling (BIM)</a>
+                    </p>
+                    <p>
+                      Oftentimes physical and functional essence of any
+                      construction project needs to be represented digitally, in
+                      a 3D model format.
+                    </p>
+                  </div>
+                </div>
+              </article>
+            </div>
+            <div className="col-md-6 col-xl-4">
+              {}
+              <article className="blurb blurb-circle">
+                <div className="unit flex-sm-row unit-spacing-md">
+                  <div className="unit__left">
+                    <div className="blurb-circle__icon">
+                      <span className="icon linear-icon-pencil-ruler2" />
+                    </div>
+                  </div>
+                  <div className="unit__body">
+                    <p className="heading-6 blurb__title">
+                      <a href="#">Design-Build</a>
+                    </p>
+                    <p>
+                      If a project is not too complex, we may hire a
+                      designer-builder type of contractor, to make the longevity
+                      of the construction shorter.
+                    </p>
+                  </div>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Services;
